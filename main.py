399fc1ff7e82612a4b63cd36872e5806f04cdd51
Shane <>
2020-11-17 21:23:54 -0500

    auto commits

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import os
 import time
 
 while True:
-    time.sleep(3)
+    time.sleep(4)
     os.system("git add .")
     time.sleep(3)
     os.system(""" git commit -m "auto commits" """)
-    os.system(""" git push """)
-    time.sleep(3)
+    os.system(""" git push https://github.com/ShaneWD/Unreal_Engine_test.git """)
+    time.sleep(10)
+    os.system(""" git clone https://github.com/ShaneWD/Unreal_Engine_test.git """)
