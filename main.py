6fc590da585c32164c73d57c881ede7e97295156
Shane <>
2020-11-19 13:51:52 -0500

    added time_chill outs

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import os
 import time
 
 while True:
-    # time.sleep(4)
+    time.sleep(4)
     os.system("git add .")
     print("git add .")
-    # time.sleep(3)
+    time.sleep(3)
     os.system("""git commit -m "auto commits" """)
     print("""git commit -m "auto commits" """)
     os.system("""git push """)
     print("""git push https://github.com/ShaneWD/Unreal_Engine_test.git """)
-    # time.sleep(10)
+    time.sleep(10)
     os.system("""git pull """)
     print("""git pull """)
     print("looping")
